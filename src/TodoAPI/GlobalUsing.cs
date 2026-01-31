12dee9dc48c7b36067c9b0e835059a54b966bac0
HelloWorld <andy28572463@gmail.com>
2026-01-31 15:43:53 +0800

    feat: 增加 OpenAPI 範例支持與擴展功能
    - 更新 Program.cs 中的 OpenApi 設定以添加範例支持。
    - 新增 ExampleOperationTransformer、ExamplesExtensions、IExampleProvider、RequestExampleAttribute 和 ResponseExampleAttribute 類別，以支持在 OpenAPI 中自動顯示請求和回應範例。

--- src/TodoAPI/GlobalUsing.cs
+++ src/TodoAPI/GlobalUsing.cs
@@ -1,4 +1,6 @@
 global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Http.HttpResults;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Scalar.AspNetCore;
 global using TodoAPI.Infrastructures.API;
--- src/TodoAPI/Program.cs
+++ src/TodoAPI/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options => options.AddExamples());
 
 builder.Services.AddEFCore(builder.Configuration);
 
