66630c4b6846772bad38ba0c1b06b4b711a759da
Jhuang <lijungjhuang@uitc.com.tw>
2026-02-06 17:21:15 +0800

    feat: 增加全新佈局和樣式文件以整合 PrimeVue 和 TailwindCSS
    - 新增全新的佈局組件，包括 AppLayout、AppSidebar、AppTopbar、AppMenu 和 AppFooter。
    - 新增多個 SCSS 文件以定義佈局樣式和變數，並整合 PrimeVue 和 TailwindCSS 的樣式。
    - 更新路由配置以使用新的佈局組件，並添加 Todo 頁面路由。
    - 在主應用中引入新的樣式和佈局，提升整體使用者體驗。

--- src/todo-web/src/main.js
+++ src/todo-web/src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css';
+import './assets/layout.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
--- src/todo-web/src/router/index.js
+++ src/todo-web/src/router/index.js
@@ -1,13 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import AppLayout from '@/layout/AppLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: AppLayout,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView,
+        },
+        {
+          path: '/todo',
+          name: 'todo',
+          component: () => import('@/views/Todo.vue'),
+        },
+      ],
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notfound',
+      component: () => import('@/views/NotFound.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+    },
+    {
+      path: '/access',
+      name: 'accessDenied',
+      component: () => import('@/views/Access.vue'),
+    },
+    {
+      path: '/error',
+      name: 'error',
+      component: () => import('@/views/Error.vue'),
     },
   ],
 });
