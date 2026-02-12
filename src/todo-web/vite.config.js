ff50e9ff17cc6dc28fa037a46c54ff2ae35a70f1
Jhuang <lijungjhuang@uitc.com.tw>
2026-02-12 18:26:57 +0800

    feat: 添加環境變數配置以支持本地開發
    - 新增 .env 和 .env.development 文件，定義 VITE_API_BASE_URL 和 VITE_WEB_BASE_URL 變數。
    - 更新 vite.config.js 以使用環境變數作為基礎 URL，提升配置靈活性。

--- src/todo-web/vite.config.js
+++ src/todo-web/vite.config.js
@@ -1,6 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import Components from 'unplugin-vue-components/vite';
@@ -8,18 +7,22 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-    }),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_WEB_BASE_URL,
+    plugins: [
+      vue(),
+      vueDevTools(),
+      Components({
+        resolvers: [PrimeVueResolver()],
+      }),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  };
 });
